c21c5b1754de18a0c3256d138145c4ee7372cd93
It423 <it42@hotmail.co.uk>
2015-05-03 11:11:34 +0100

    Create base UI

--- Minesweeper/Minesweeper/MainWindow.xaml.cs
+++ Minesweeper/Minesweeper/MainWindow.xaml.cs
@@ -1,17 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// MainWindow.xaml.cs
+// <copyright file="MainWindow.cs"> This code is protected under the MIT License. </copyright>
+using System;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Minesweeper
 {
@@ -20,9 +13,92 @@ namespace Minesweeper
     /// </summary>
     public partial class MainWindow : Window
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainWindow" /> class.
+        /// </summary>
         public MainWindow()
         {
-            InitializeComponent();
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Gets or sets the tiles for minesweeper.
+        /// </summary>
+        public Button[][] Tiles { get; set; }
+
+        /// <summary>
+        /// Initializes a minesweeper grid.
+        /// </summary>
+        /// <param name="rows"> The amount of rows in the grid. </param>
+        /// <param name="columns"> The amount of columns in the grid. </param>
+        private void InitializeGrid(int rows, int columns)
+        {
+            // Set window size
+            this.Width = 16 * columns + 20;
+            this.Height = 16 * rows + 130;
+
+            // Set the rows and columns on the display grid
+            this.SetGridDefinitions(rows, columns);
+
+            // Generate the grid
+            this.Tiles = new Button[columns][];
+            for (int x = 0; x < columns; x++)
+            {
+                this.Tiles[x] = new Button[rows];
+                for (int y = 0; y < rows; y++)
+                {
+                    this.CreateTileButton(x, y);
+                }
+            }
+
+            this.MineGrid.UpdateLayout();
+        }
+
+        /// <summary>
+        /// Creates a tile button. 
+        /// </summary>
+        /// <param name="x"> The x position of the tile. </param>
+        /// <param name="y"> The y position of the tile. </param>
+        private void CreateTileButton(int x, int y)
+        {
+            this.Tiles[x][y] = new Button();
+
+            // Set the image
+            ImageBrush brush = new ImageBrush();
+            brush.ImageSource = new BitmapImage(new Uri("Images/Covered.png", UriKind.Relative));
+            this.Tiles[x][y].Background = brush;
+
+            // Set the size
+            this.Tiles[x][y].Width = 16;
+            this.Tiles[x][y].Height = 16;
+
+            // Set the grid position and add to grid
+            Grid.SetColumn(this.Tiles[x][y], x);
+            Grid.SetRow(this.Tiles[x][y], y);
+            this.MineGrid.Children.Add(this.Tiles[x][y]);
+        }
+
+        /// <summary>
+        /// Sets the mine grid to the right size.
+        /// </summary>
+        /// <param name="rows"> The amount of rows. </param>
+        /// <param name="columns"> The amount of columns. </param>
+        private void SetGridDefinitions(int rows, int columns)
+        {
+            this.MineGrid.RowDefinitions.Clear();
+            this.MineGrid.ColumnDefinitions.Clear();
+
+            for (int i = 0; i < rows; i++)
+            {
+                this.MineGrid.RowDefinitions.Add(new RowDefinition());
+                this.MineGrid.RowDefinitions[i].Height = new GridLength(1, GridUnitType.Star);
+            }
+
+            for (int i = 0; i < columns; i++)
+            {
+                this.MineGrid.ColumnDefinitions.Add(new ColumnDefinition());
+                this.MineGrid.ColumnDefinitions[i].Width = new GridLength(1, GridUnitType.Star);
+            }
         }
     }
 }
